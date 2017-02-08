a1fc37bd45d5f8e49d66258fe1ec05f276ff5ae7
Aaron Rhodes <aaroncraigrhodes@googlemail.com>
2017-02-08 14:11:29 +0000

    Save changes - not working

--- src/SFA.DAS.RAA.Api.Service.V1/Vacancy/ApiVacancySummaryService.cs
+++ src/SFA.DAS.RAA.Api.Service.V1/Vacancy/ApiVacancySummaryService.cs
@@ -3,26 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFA.Apprenticeships.Application.Interfaces;
+using SFA.Apprenticeships.Application.Interfaces.Api;
+using SFA.Apprenticeships.Application.Interfaces.Service;
 using SFA.Apprenticeships.Application.Interfaces.Vacancy;
 using SFA.Apprenticeships.Application.Vacancy;
 using SFA.Apprenticeships.Domain.Entities.Raa.Vacancies;
 using SFA.Apprenticeships.Domain.Raa.Interfaces.Queries;
 using SFA.Apprenticeships.Domain.Raa.Interfaces.Repositories;
 using SFA.Apprenticeships.Domain.Raa.Interfaces.Repositories.Models;
+using SFA.DAS.RAA.Api.Service.V1.Mappers;
 
 namespace SFA.DAS.RAA.Api.Service.V1.Vacancy
 {
     class ApiVacancySummaryService : IVacancySummaryService
     {
+        private static readonly IMapper ApiClientMappers = new ApiClientMappers();
+
+        private readonly IApiClientProvider _apiClientProvider;
         private readonly IVacancySummaryService _vacancySummaryService;
 
-        public ApiVacancySummaryService(IVacancySummaryRepository vacancySummaryRepository)
+        public ApiVacancySummaryService(IApiClientProvider apiClientProvider, 
+            IVacancySummaryRepository vacancySummaryRepository)
         {
+            _apiClientProvider = apiClientProvider;
             _vacancySummaryService = new VacancySummaryService(vacancySummaryRepository);
         }
 
-        public IList<VacancySummary> GetSummariesForProvider(VacancySummaryQuery query, out int totalRecords)
+        public ListWithTotal<VacancySummary> GetSummariesForProvider(VacancySummaryQuery query, out int totalRecords)
         {
+            if (_apiClientProvider.IsEnabled())
+            {
+                var apiClient = _apiClientProvider.GetApiClient();
+
+                var apiVacancyResult = Task.Run(() => apiClient.VacancySummaryOperations.GetVacancySummariesWithHttpMessagesAsync(
+                    query.SearchString,
+                    query.SearchMode.ToString(),
+                    query.VacancyType.ToString(),
+                    query.Order.ToString(),
+                    query.OrderByField.ToString(),
+                    query.Filter.ToString(),
+                    query.RequestedPage,
+                    query.PageSize
+                    )).Result;
+
+                if (apiVacancyResult.Response.IsSuccessStatusCode)
+                {
+                    return new ServiceResult<WageUpdate>(VacancyManagementServiceCodes.EditWage.Ok, apiVacancyResult);
+                }
+
+                return new ServiceResult<WageUpdate>(VacancyManagementServiceCodes.EditWage.Error, apiVacancyResult);
+            }
+
             return _vacancySummaryService.GetSummariesForProvider(query, out totalRecords);
         }
 
