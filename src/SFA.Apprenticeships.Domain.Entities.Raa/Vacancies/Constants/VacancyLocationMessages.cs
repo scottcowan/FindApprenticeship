2a57f2b9ece4176efe182de73c32e1639d6d6b8c
David Winchurch <david.winchurch@iterativeit.com>
2017-02-10 08:30:00 +0000

    RA-607: Vacancy location validation

--- src/SFA.Apprenticeships.Domain.Entities.Raa/Vacancies/Constants/VacancyLocationMessages.cs
+++ src/SFA.Apprenticeships.Domain.Entities.Raa/Vacancies/Constants/VacancyLocationMessages.cs
@@ -2,6 +2,19 @@
 {
     public class VacancyLocationMessages
     {
-        
+        public static class Address
+        {
+            public const string RequiredErrorText = "Please supply an address for this vacancy location.";
+        }
+
+        public class NumberOfPositions
+        {
+            public const string LengthErrorText = "There must be at least 1 position for this vacancy location.";
+        }
+
+        public static class EmployersWebsite
+        {
+            public const string InvalidUrlText = "Please supply a valid website url for the employer. This is the link that will be used to apply on the employer's website if the vacancy is set as offline.";
+        }
     }
 }
---
+++ src/SFA.DAS.RAA.Api.UnitTests/Validators/PostalAddressValidatorTests.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.DAS.RAA.Api.UnitTests.Validators
+{
+    using Api.Validators;
+    using NUnit.Framework;
+
+    [TestFixture]
+    [Parallelizable]
+    public class PostalAddressValidatorTests
+    {
+        private readonly PostalAddressValidator _postalAddressValidator = new PostalAddressValidator();
+
+    }
+}
